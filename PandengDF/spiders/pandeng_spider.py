e2a29a2970787b800e893a9cbf6f44a974fff806
biligo <1534761000@qq.com>
2019-08-22 14:49:05 +0800

    '优化爬虫'

--- PandengDF/spiders/pandeng_spider.py
+++ PandengDF/spiders/pandeng_spider.py
@@ -4,15 +4,33 @@
 # @Email   : lessguo@163.com
 # @File    : pandeng_spider.py
 # @Software: PyCharm
+from email import header
 
 import scrapy
 from PandengDF.items import PandengItem
 import json
 import time
+import requests
+
+
+# def books_list():
+#     total_count = 238
+#     pagesize = total_count
+#     url = 'https://api.dushu.io/books'
+#     data = {
+# 	"bookReadStatus": -1,
+# 	"order": 1,
+# 	"pageSize": 10,
+# 	"token": "qqhQDZLZVRbLM8EOthX",
+# 	"page": 1
+# }
+#
+#     res = requests.post(url=url, data=data)
+#     print(res.text)
 
 
 def books_video():
-    with open('/Users/guo/PycharmProjects/PandengDF/books_list_fragmentId.json', 'r', encoding='utf-8') as f:
+    with open('D:\list\\tt3.json', 'r', encoding='utf-8') as f:
         dict = json.load(f)
 
     ll = dict['books']
@@ -20,11 +38,15 @@ def books_video():
     for item in ll:
         dic = item['contents']
         for it in dic:
-            if it['type'] == 3:
-                lis.append(it['fragmentId'])
+            # it['type'] == 2 音频
+            # it['type'] == 3 视频
+            if it['type'] == 2:
+                if 'fragmentId' in it.keys():
+                    lis.append(it['fragmentId'])
 
-    return lis
-    # print(lis)
+    # return lis
+    print(lis)
+
 
 class PandengSpider(scrapy.Spider):
     name = 'pandeng'
@@ -34,30 +56,24 @@ class PandengSpider(scrapy.Spider):
     # ]
     ll = books_video()
 
-
-
     def start_requests(self):
         url = 'http://api.dushu.io/fragment/content'
 
-
         for line in self.ll:
-
             yield scrapy.FormRequest(
                 url=url,
                 headers={'Content-Type': 'application/x-www-form-urlencoded',
-                         'Accept': ''},
+                         'Accept': '',
+                         'x-dushu-app-ver': '3.9.49'},
                 formdata={
-                    'token': 'c7O3gSN0FJswegpQtOV',
+                    'token': 'qqhQDZLZVRbLM8EOthX',
                     'fragmentId': str(line)
                 },
                 callback=self.parse
             )
 
-
-
-
     def parse(self, response):
-
+        # print('+++++++++++++++', response.text)
         jsobj = json.loads(response.body)
         title = jsobj['title']
         summary = jsobj['summary']
